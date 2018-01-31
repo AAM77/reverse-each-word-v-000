99bee833672624b8f17b70029f130bd7ce433ed0
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-01-31 18:16:10 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,7 @@ def reverse_each_word(string)
 
   string.split(" ").collect {|word| word.reverse}.join(" ")
 
+def reverse_each_word2(string)
     string_array = string.split(" ")
     reverse_array = [ ]
 
