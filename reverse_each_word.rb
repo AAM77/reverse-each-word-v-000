a0f974a730fc94846b735b358eb1d8845860f3a3
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-01-31 18:31:55 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,19 +1,20 @@
-
-def reversed (word)
-  count = word.length - 1
-  reverse_word = ""
-
-  while (count >= 0)
-    reverse_word = "#{reverse_word}#{word[count]}"
-    count -= 1
-  end
-end
-
 def reverse_each_word(string)
     string_array = string.split(" ")
     reverse_array = [ ]
 
     string_array.collect { |word|
-      reverse_array.push(word.reversed)
-    }.join(" ")
+      count = word.length - 1
+      reverse_word = ""
+
+      while (count >= 0)
+        reverse_word = "#{reverse_word}#{word[count]}"
+        count -= 1
+      end
+
+      reverse_array.push(reverse_word)
+    end
+
+    new_string = reverse_array.join(" ")
+
+    return (new_string)
 end
