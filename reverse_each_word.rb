75918d318a9ff331d618da3ace8a48a81856ff14
Mohammad Adeel <adeel.a.mhd@gmail.com>
2018-01-31 18:15:31 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -13,9 +13,7 @@ def reverse_each_word(string)
         reverse_word = "#{reverse_word}#{word[count]}"
         count -= 1
       end
-
-      reverse_array.push(reverse_word)
-    end
+    }.join(" ")
 
     new_string = reverse_array.join(" ")
 
